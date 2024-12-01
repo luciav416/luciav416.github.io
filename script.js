02e9e883bfebcf726295d74b86b8390f77ddbcfb
Y0jojojo <160549451+Y0jojojo@users.noreply.github.com>
2024-11-30 23:15:08 -0500

    30 nov
    Co-Authored-By: Andrea Lucia Vanegas Castro <vanea718@newschool.edu>

---
+++ script.js
@@ -0,0 +1,12 @@
+/* const header = document.querySelector(".full-header");
+const scrollText = document.getElementById("scroll-text")
+
+document.addEventListener("scroll", () => {
+    if (window.screenY > 50) {
+        header.classList.add("shrink-header")
+        scrollText.classList.add("hide-scroll")
+    } else {
+        header.classList.remove("shrink-header")
+        scrollText.classList.remove("hide-scroll")
+    }
+}) */
