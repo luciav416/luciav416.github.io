a5f160839de597f97bf9f72de0eef07e3b067e55
Y0jojojo <160549451+Y0jojojo@users.noreply.github.com>
2024-11-30 23:48:57 -0500

    Nov 30

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-/* const header = document.querySelector(".full-header");
+const header = document.getElementById("full-header");
+const headerText = document.getElementById("header-text")
 const scrollText = document.getElementById("scroll-text")
+window.onscroll = function () { scrollFunction() };
 
-document.addEventListener("scroll", () => {
-    if (window.screenY > 50) {
-        header.classList.add("shrink-header")
-        scrollText.classList.add("hide-scroll")
+function scrollFunction() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        header.style.height = "20vh"
+        header.style.backgroundColor = "black"
+        header.style.backgroundImage = "none"
+        headerText.style.fontSize = "7rem"
     } else {
-        header.classList.remove("shrink-header")
-        scrollText.classList.remove("hide-scroll")
+        header.style.height = "100vh"
+        header.style.backgroundImage = "url(./assets/Imagen_rio-magdalena.jpg)"
     }
-}) */
+}
