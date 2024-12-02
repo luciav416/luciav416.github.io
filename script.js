5d8423de424f613af0ebdfed652ba25cd5a57cee
Y0jojojo <160549451+Y0jojojo@users.noreply.github.com>
2024-12-02 01:31:15 -0500

    Nov 12

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-const header = document.getElementById("full-header");
+const header = document.getElementById("header")
 const headerText = document.getElementById("header-text")
 const scrollText = document.getElementById("scroll-text")
-window.onscroll = function () { scrollFunction() };
+const textInformation = document.getElementById("text-information")
+const entries = document.getElementById("entries")
 
+window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-        header.style.height = "20vh"
-        header.style.backgroundColor = "black"
-        header.style.backgroundImage = "none"
-        headerText.style.fontSize = "7rem"
-    } else {
-        header.style.height = "100vh"
-        header.style.backgroundImage = "url(./assets/Imagen_rio-magdalena.jpg)"
+    if (window.innerWidth > 768) {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            header.style.height = "200px"
+            /* header.style.position = "fixed" */
+            headerText.style.fontSize = "8rem"
+            headerText.style.paddingTop = "25px"
+            scrollText.style.display = "none"
+            /* textInformation.style.paddingTop = "200px"
+            entries.style.paddingTop = "200px" */
+        } else {
+            /* header.style.height = "100vh"
+            header.style.position = "relative"
+            headerText.style.fontSize = "14rem"
+            headerText.style.paddingTop = "0px"
+            scrollText.style.display = "block"
+            textInformation.style.paddingTop = "0px"
+            entries.style.paddingTop = "0px" */
+        }
     }
 }
+
