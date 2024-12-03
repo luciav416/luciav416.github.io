9504e2761af54f0e9acb05045ae969c44803b441
Andrea Lucia Vanegas Castro <vanea718@newschool.edu>
2024-12-03 15:10:11 -0500

    3 Dec

--- script.js
+++ script.js
@@ -7,22 +7,19 @@ const entries = document.getElementById("entries")
 window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     if (window.innerWidth > 768) {
-        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-            header.style.height = "200px"
-            header.style.position = "fixed"
-            headerText.style.fontSize = "8rem"
-            headerText.style.paddingTop = "25px"
-            scrollText.style.display = "none"
-            textInformation.style.paddingTop = "200px"
-            entries.style.paddingTop = "200px"
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;  
+        if (scrollTop > 50 ) {
+            header.style.height = "200px";
+            header.style.position = "fixed";
+            headerText.style.fontSize = "8rem";
+            headerText.style.paddingTop = "25px";
+            scrollText.style.display = "none";
         } else {
-            /* header.style.height = "100vh"
-            header.style.position = "relative"
-            headerText.style.fontSize = "14rem"
-            headerText.style.paddingTop = "0px"
-            scrollText.style.display = "block"
-            textInformation.style.paddingTop = "0px"
-            entries.style.paddingTop = "0px" */
+            header.style.height = "100vh";
+            header.style.position = "fixed";
+            headerText.style.fontSize = "14rem";
+            headerText.style.paddingTop = "0px";
+            scrollText.style.display = "block";
         }
     }
 }
