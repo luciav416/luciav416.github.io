b66b42c053aab2d06a3596f5e0cde853cdd0455e
Y0jojojo <160549451+Y0jojojo@users.noreply.github.com>
2024-12-03 16:00:23 -0500

    3 dec

--- entry-3/script.js
+++ entry-3/script.js
@@ -2,9 +2,9 @@ let imgs = document.querySelectorAll("img");
 
 function applyRandomRotation(){
     imgs.forEach(function(element){
-        let randomAngle = Math.floor(Math.random() * 30);
+        let randomAngle = Math.floor(Math.random() * 30) - 15;
         element.style.transform = 'rotate(' + randomAngle + 'deg)';
     });
 }
 
-setInterval(applyRandomRotation, 500); 
+setInterval(applyRandomRotation, 200); 
