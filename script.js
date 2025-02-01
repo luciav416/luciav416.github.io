720c80527b217a730094078b1c35f77b6314f28e
Y0jojojo <160549451+Y0jojojo@users.noreply.github.com>
2025-02-01 14:08:53 -0500

    a

--- script.js
+++ script.js
@@ -8,17 +8,14 @@ window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     if (window.innerWidth > 768) {
         const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;  
-        if (scrollTop > 50 ) {
+        if (scrollTop > 0 ) {
             header.style.height = "200px";
-            header.style.position = "fixed";
+            header.style.position = "relative";
             headerText.style.fontSize = "8rem";
-            headerText.style.paddingTop = "25px";
             scrollText.style.display = "none";
         } else {
             header.style.height = "100vh";
-            header.style.position = "fixed";
             headerText.style.fontSize = "14rem";
-            headerText.style.paddingTop = "0px";
             scrollText.style.display = "block";
         }
     }
